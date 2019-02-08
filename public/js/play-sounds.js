86fd0c4a4b489436438cb010f77b142259d528f3
Jackieh55555 <jackie.huang.ca@gmail.com>
2019-02-08 18:21:24 -0500

    sounds

---
+++ public/js/play-sounds.js
@@ -0,0 +1,31 @@
+AFRAME.registerComponent('play-music', {
+    schema: {},
+    init : function() {
+        const Context_AF = this;
+        Context_AF.soundElem = document.querySelector('#playSound');
+
+        Context_AF.el.addEventListener('click', function(event) {
+            console.log("click");
+            //object clicked - lets create a cow!
+
+            Context_AF.soundElem.components['sound'].stopSound(); //stop first so we aren't trying to play more than once at same time
+            Context_AF.soundElem.components['sound'].playSound();
+        });
+    },
+});
+
+AFRAME.registerComponent('play-effect', {
+    schema: {},
+    init : function() {
+        const Context_AF = this;
+        Context_AF.soundElem = document.querySelector('#playEffect');
+
+        Context_AF.el.addEventListener('click', function(event) {
+            console.log("click");
+            //object clicked - lets create a cow!
+
+            Context_AF.soundElem.components['sound'].stopSound(); //stop first so we aren't trying to play more than once at same time
+            Context_AF.soundElem.components['sound'].playSound();
+        });
+    },
+});
