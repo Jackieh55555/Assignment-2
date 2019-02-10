8c99dc0fee3b4139413df0eafa7fe7b283ad8257
Jackieh55555 <jackie.huang.ca@gmail.com>
2019-02-10 13:48:25 -0500

    final changes

--- public/js/create-2nd-object.js
+++ public/js/create-2nd-object.js
@@ -6,7 +6,7 @@ AFRAME.registerComponent('create-tv-screen', {
 
         Context_AF.el.addEventListener('click', function(event) {
             console.log("click");
-            //object clicked - lets create a cow!
+            //object clicked - image will appera on TV
             Context_AF.createScreen();
 
             Context_AF.soundElem.components['sound'].stopSound(); //stop first so we aren't trying to play more than once at same time
@@ -15,11 +15,6 @@ AFRAME.registerComponent('create-tv-screen', {
     },
     createScreen : function() {
         const Context_AF = this;
-
-        //create element, than add attributes you want. If you are creating many =you want to 
-        //consider "pooling" elements (i.e. not creating/deleting a bunch but just hiding/showing a certain MAX amount with visibility="true" or "false" )
-        //see here: https://www.html5rocks.com/en/tutorials/speed/static-mem-pools/ 
-        //see here: https://aframe.io/docs/0.8.0/components/pool.html
         let screenElem = document.createElement('a-entity');
         screenElem.setAttribute('obj-model', {obj:'/assets/models/TVScreen.obj'});
         screenElem.setAttribute('material', {src:'/assets/textures/TVScreen.png'});
@@ -40,7 +35,7 @@ AFRAME.registerComponent('create-fireplace', {
 
         Context_AF.el.addEventListener('click', function(event) {
             console.log("click");
-            //object clicked - lets create a cow!
+            //object clicked - image will appear on fireplace
             Context_AF.createFireplaceScreen();
 
             Context_AF.soundElem.components['sound'].stopSound(); //stop first so we aren't trying to play more than once at same time
@@ -49,11 +44,6 @@ AFRAME.registerComponent('create-fireplace', {
     },
     createFireplaceScreen: function() {
         const Context_AF = this;
-
-        //create element, than add attributes you want. If you are creating many =you want to 
-        //consider "pooling" elements (i.e. not creating/deleting a bunch but just hiding/showing a certain MAX amount with visibility="true" or "false" )
-        //see here: https://www.html5rocks.com/en/tutorials/speed/static-mem-pools/ 
-        //see here: https://aframe.io/docs/0.8.0/components/pool.html
         let fireElem = document.createElement('a-entity');
         fireElem.setAttribute('obj-model', {obj:'/assets/models/FireplaceScreen.obj'});
         fireElem.setAttribute('material', {src:'/assets/textures/FireplaceScreen.png'});
